eeee954c9ead851812e9e28c4dd9690435df5bd4
Anuj Chaudhary <chaudharyanuj0123@gmail.com>
2026-03-31 10:21:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT ;
 
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(cors({ origin:  "https://link-hub-snowy.vercel.app/" }));
 app.use(express.json());
 app.use(morgan("dev"));
 
